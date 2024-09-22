4f37e0d34a55670ccca55a37ce97f1fe14863916
EileenNegahdary <aylinnegahdari1380@gmail.com>
2024-09-22 22:01:15 +0330

    get user's current location and organize the homepage to show the nearby restaurants

--- foodOnline_main/views.py
+++ foodOnline_main/views.py
@@ -1,11 +1,27 @@
 from django.http import request
 from django.shortcuts import render
 
+from django.db.models import Q
+from django.contrib.gis.geos import GEOSGeometry
+from django.contrib.gis.measure import D
+from django.contrib.gis.db.models.functions import Distance
+
 from vendor.models import Vendor
-
-
+from .utils import get_or_set_current_location
+    
 def home(request):
-    vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)[:8]
+    if get_or_set_current_location(request) is not None:
+        
+        pnt = GEOSGeometry('POINT(%s %s)' % (get_or_set_current_location(request)))
+
+        vendors = Vendor.objects.filter(user_profile__location__distance_lte=(pnt, D(km=100))).annotate(distance=Distance("user_profile__location", pnt)).order_by("distance")
+
+        for v in vendors:
+            v.kms = round(v.distance.km, 1)
+
+    else:
+        vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)[:8]
+        
     context = {
         'vendors': vendors,
     }
